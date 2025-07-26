63a623be9e674e1c5158553de825852c17dd7d89
ahmedhasssan1 <ahmedkhel2003@gmail.com>
2025-07-26 19:51:52 +0300

    update the photo tag service

--- src/photo_tags/photo_tags.service.ts
+++ src/photo_tags/photo_tags.service.ts
@@ -9,6 +9,7 @@ import { Repository } from 'typeorm';
 import { PhotoTagsDto } from './dto/photoTags.dto';
 import { UsersService } from 'src/users/users.service';
 import { PostsService } from 'src/posts/posts.service';
+import { knex } from 'src/knex';
 
 @Injectable()
 export class PhotoTagsService {
@@ -28,25 +29,39 @@ export class PhotoTagsService {
     if (!findPost) {
       throw new NotFoundException('Post not found');
     }
-    const findTag = await this.PhotoTageRepo.findOne({where:{user:{id:photoTag.userId},post:{id:photoTag.postId}},relations:['user','post']});
+    const isFollowing = await knex('followers')
+      .where({
+        leader_id: findPost.user.id,
+        follower_id: findUser.id,
+      })
+      .first();
+    if (!isFollowing) {
+      throw new BadRequestException('this user not followed the post owner');
+    }
+    const findTag = await this.PhotoTageRepo.findOne({
+      where: { user: { id: photoTag.userId }, post: { id: photoTag.postId } },
+      relations: ['user', 'post'],
+    });
     if (findTag) {
       throw new BadRequestException(
         'this user already you meantion him in this post',
       );
     }
     const newTag = await this.PhotoTageRepo.create({
-        post:findPost,
-        user:findUser,
-        x:photoTag.x,
-        y:photoTag.y
+      post: findPost,
+      user: findUser,
+      x: photoTag.x,
+      y: photoTag.y,
     });
     return await this.PhotoTageRepo.save(newTag);
-
   }
-  async findPostTags(postId:number):Promise<PhotoTags[]>{
-    const findTags=await this.PhotoTageRepo.find({where:{post:{id:postId}},relations:['post','user']});
-    if(!findTags){
-        throw new NotFoundException("there is no tags  with  this  post")
+  async findPostTags(postId: number): Promise<PhotoTags[]> {
+    const findTags = await this.PhotoTageRepo.find({
+      where: { post: { id: postId } },
+      relations: ['post', 'user'],
+    });
+    if (!findTags) {
+      throw new NotFoundException('there is no tags  with  this  post');
     }
     return findTags;
   }
